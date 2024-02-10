37145d9e4405008e6e9e98d778a84f2676a0aa79
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-02-10 12:08:16 +0100

    first commit

---
+++ delimiters.js
@@ -0,0 +1,27 @@
+"use strict";
+// var hashes = ['# ', ' #'];
+var slashes = ['/* ', ' */'];
+// var semicolons = [';; ', ' ;;'];
+// var parens = ['(* ', ' *)'];
+// var dashes = ['-- ', ' --'];
+// var percents = ['%% ', ' %%'];
+exports.languageDemiliters = {
+    'c': slashes,
+    'cpp': slashes,
+    'css': slashes,
+    'go': slashes,
+    'jade': slashes,
+    'java': slashes,
+    'javascript': slashes,
+    'javascriptreact': slashes,
+    'less': slashes,
+    'objective-c': slashes,
+    'php': slashes,
+    'rust': slashes,
+    'scss': slashes,
+    'swift': slashes,
+    'typescript': slashes,
+    'typescriptreact': slashes,
+    'xsl': slashes,
+};
+//# sourceMappingURL=delimiters.js.map
---
+++ extension.js
@@ -0,0 +1,64 @@
+const vscode = require('vscode');
+var delimiters_1 = require("./delimiters");
+
+let header = `/**!
+
+
+
+
+
+
+ __  __ 
+|  \\/  |   __ _  __  __   ___   _ __     ___    ___     / ___|   __ _   _ __ ___     __ _ 
+| |\\/| |  / _\` | \\ \\/ /  / _ \\ | '_ \\   / __|  / _ \\   | |  _   / _\` | | '_ \` _ \\   / _\` |
+| |  | | | (_| |  >  <  |  __/ | | | | | (__  |  __/   | |_| | | (_| | | | | | | | | (_| |
+|_|  |_|  \\__,_| /_/\\_\\  \\___| |_| |_|  \\___|  \\___|    \\____|  \\__,_| |_| |_| |_|  \\__,_|
+
+
+
+
+
+
+
+ */
+/**!
+ *   @license © Copyright {{YEAR}}, All rights reserved.
+ *   @author Maxence Gama, @maxencegama
+ *   @contact contact@maxencegama.dev
+ */
+\n`;
+
+let supportsLanguage = function (languageId) {
+    return languageId in delimiters_1.languageDemiliters;
+};
+
+let printHeader = () => {
+	var activeTextEditor = vscode.window.activeTextEditor;
+	if (!activeTextEditor) {
+		return;
+	}
+    var document = activeTextEditor.document;
+	if (supportsLanguage(document.languageId)) {
+		let textheader = header.replace("{{YEAR}}", new Date().getFullYear());
+		activeTextEditor.edit(function (editor) {
+			editor.insert(new vscode.Position(0, 0), textheader);
+		});
+	}
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let disposable = vscode.commands.registerCommand('mg-header.printheader', printHeader);
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
